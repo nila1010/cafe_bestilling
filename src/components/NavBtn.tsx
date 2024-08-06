3029130a249b7871c5ffe90ee670314468840f4d
NL <nila0001@stud.kea.dk>
2024-08-06 17:00:18 +0200

    prefetch

--- src/components/NavBtn.tsx
+++ src/components/NavBtn.tsx
@@ -3,6 +3,7 @@ import Link from "next/link";
 export default function NavBtn({ children, linkHref }: { children: React.ReactNode; linkHref: string }) {
   return (
     <Link
+      prefetch={false}
       className="font-bold text-2xl"
       href={`/${linkHref}`}>
       {children}
