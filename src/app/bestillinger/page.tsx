4ee2b279dc83e480e19d794c7d7447a167fce41b
NL <nila0001@stud.kea.dk>
2024-08-06 17:03:43 +0200

    remove force dynamic

--- src/app/bestillinger/page.tsx
+++ src/app/bestillinger/page.tsx
@@ -1,6 +1,5 @@
 import Orderlistitem from "@/components/Orderlistitem";
 import prisma from "@/lib/db";
-export const dynamic = "force-dynamic";
 
 export default async function bestilt() {
   const data = await prisma.bestilling.findMany({
