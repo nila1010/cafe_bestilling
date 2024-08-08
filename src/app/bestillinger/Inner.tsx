91597b8553e0d049a54cc155bfcdc23ab113c2c7
NL <nila0001@stud.kea.dk>
2024-08-08 16:02:51 +0200

    login

---
+++ src/app/bestillinger/Inner.tsx
@@ -0,0 +1,66 @@
+"use client";
+import Orderlistitem from "@/components/Orderlistitem";
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import { Prisma } from "@prisma/client";
+import { useState } from "react";
+
+type InnerProps = Prisma.BestillingGetPayload<{
+  include: {
+    orders: true;
+  };
+}>[];
+
+export default function Inner({ data }: { data: InnerProps }) {
+  const [login, setLogin] = useState(false);
+
+  const filteredData = data.map((oneOrder) => ({
+    ...oneOrder,
+    orders: oneOrder.orders.filter((order) => !order.completed),
+  }));
+
+  function checkLogin(e: React.FormEvent<HTMLFormElement>) {
+    e.preventDefault();
+    if (e.currentTarget.password.value === "1234") {
+      setLogin(true);
+    }
+  }
+
+  return (
+    <article>
+      {!login ? (
+        <>
+          <h1 className="text-2xl mt-5">Login for at se lister</h1>
+          <form
+            onSubmit={checkLogin}
+            className="mt-5">
+            <Label>
+              Password
+              <Input
+                name="password"
+                type="password"
+              />
+            </Label>
+            <Button className="mt-3">Login</Button>
+          </form>
+        </>
+      ) : (
+        <>
+          <h1 className="text-2xl mt-5">Oversigt over ting som skal bestilles</h1>
+          {filteredData.map((oneOrder) =>
+            oneOrder.orders.length > 0 ? (
+              <Orderlistitem
+                key={oneOrder.orderId}
+                orderData={oneOrder.orders}
+                initials={oneOrder.initials}
+                orderDate={oneOrder.orderDate}
+                orderId={oneOrder.orderId}
+              />
+            ) : null
+          )}
+        </>
+      )}
+    </article>
+  );
+}
--- src/app/bestillinger/page.tsx
+++ src/app/bestillinger/page.tsx
@@ -1,4 +1,4 @@
-import Orderlistitem from "@/components/Orderlistitem";
+import Inner from "./Inner";
 import prisma from "@/lib/db";
 export const dynamic = "force-dynamic";
 
@@ -9,25 +9,5 @@ export default async function bestilt() {
     },
   });
 
-  const filteredData = data.map((oneOrder) => ({
-    ...oneOrder,
-    orders: oneOrder.orders.filter((order) => !order.completed),
-  }));
-
-  return (
-    <article>
-      <h1 className="text-2xl mt-5">Oversigt over ting som skal bestilles</h1>
-      {filteredData.map((oneOrder) =>
-        oneOrder.orders.length > 0 ? (
-          <Orderlistitem
-            key={oneOrder.orderId}
-            orderData={oneOrder.orders}
-            initials={oneOrder.initials}
-            orderDate={oneOrder.orderDate}
-            orderId={oneOrder.orderId}
-          />
-        ) : null
-      )}
-    </article>
-  );
+  return <Inner data={data} />;
 }
