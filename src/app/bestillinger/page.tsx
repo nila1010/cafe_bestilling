f3a6c052dcef93051e6c2070d2b4fa991963effe
NL <nila0001@stud.kea.dk>
2024-08-14 20:07:32 +0200

    dynamic

--- src/app/bestillinger/page.tsx
+++ src/app/bestillinger/page.tsx
@@ -1,5 +1,6 @@
 import Inner from "./Inner";
 import prisma from "@/lib/db";
+export const dynamic = "force-dynamic";
 
 export default async function bestilt() {
   const data = await prisma.bestilling.findMany({
