05ff89912afb73a3fd938d4645a52c204c100a7b
NL <nila0001@stud.kea.dk>
2024-08-17 19:21:44 +0200

    style

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,13 +1,8 @@
-import type { Config } from "tailwindcss"
+import type { Config } from "tailwindcss";
 
 const config = {
   darkMode: ["class"],
-  content: [
-    './pages/**/*.{ts,tsx}',
-    './components/**/*.{ts,tsx}',
-    './app/**/*.{ts,tsx}',
-    './src/**/*.{ts,tsx}',
-	],
+  content: ["./pages/**/*.{ts,tsx}", "./components/**/*.{ts,tsx}", "./app/**/*.{ts,tsx}", "./src/**/*.{ts,tsx}"],
   prefix: "",
   theme: {
     container: {
@@ -67,14 +62,20 @@ const config = {
           from: { height: "var(--radix-accordion-content-height)" },
           to: { height: "0" },
         },
+        grow: {
+          "0%": { transform: "scale(1)" },
+          "50%": { transform: "scale(1.4)" },
+          "100%": { transform: "scale(1)" },
+        },
       },
       animation: {
+        grow: "grow 1s ease",
         "accordion-down": "accordion-down 0.2s ease-out",
         "accordion-up": "accordion-up 0.2s ease-out",
       },
     },
   },
   plugins: [require("tailwindcss-animate")],
-} satisfies Config
+} satisfies Config;
 
-export default config
+export default config;
